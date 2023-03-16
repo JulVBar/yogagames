b15b0cb44518f50bbab4faeef5eda2ce8458a87c
Jul Bar <barankovauv@gmail.com>
2023-03-16 10:35:24 +0300

    header+hero+hero animation text

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@
 import './styles/main.scss';
 import preloader from './js/preloader';
 import btnToTop from './js/btn-to-top';
+import main from './js/main';
 
 
 window.addEventListener('DOMContentLoaded', function() {
-    // preloader();
+    preloader();
     btnToTop();
+    main();
+    
 });
 
 // load all images
 //каждую подпапку импортируем отдельно (иконки просто копируются в конфиге)
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg|gif)$/));
-const slider = importAll(require.context('./images/slider', false, /\.(png|jpe?g|svg|gif)$/));
 const logo = importAll(require.context('./images/logo', false, /\.(png|jpe?g|svg|gif)$/));
+const hero = importAll(require.context('./images/hero', false, /\.(png|jpe?g|svg|gif)$/));
+
+
 
 function importAll(r) {
     let images = {};
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,17 +1,22 @@
 function preloader() {
-
     document.body.onload = function() {
-        document.body.style.overflow = 'hidden';
-        
+        // document.body.style.overflow = 'hidden';
         setTimeout(function() {
-            let preloader = document.querySelector('.preloader');
+            // let preloader = document.querySelector('.preloader');
     
-            if (preloader && !preloader.classList.contains('done')) {
-                preloader.classList.add('done');
-                document.body.style.overflow = '';
-            }
+            // if (preloader && !preloader.classList.contains('done')) {
+            //     preloader.classList.add('done');
+            //     document.body.style.overflow = '';
+            // }
 
-        }, 1000); //$preloader-delay: 1000ms in SCSS
+            const animatedElems = document.querySelectorAll('[data-animation]');
+            
+            if (animatedElems && animatedElems?.length > 0) {
+                if (window.pageYOffset === 0) {
+                    animatedElems.forEach(item=>item.classList.add('start-animation'));
+                }
+            }
+        }, 0);
     };
     
 }
