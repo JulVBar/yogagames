26362c87fb47a403b6a1733c36792b43a0438f2c
Jul Bar <barankovauv@gmail.com>
2023-05-27 17:08:10 +0300

    iframe

--- src/js/videoSlider.js
+++ src/js/videoSlider.js
@@ -7,35 +7,35 @@ function videoSlider() {
     },
   });
 
-  const videoWrappers = document.querySelectorAll('.video');
+  // const videoWrappers = document.querySelectorAll('.video');
 
-  videoWrappers?.forEach(videoWrapper => {
-    videoPlayer(videoWrapper);
-  });
+  // videoWrappers?.forEach(videoWrapper => {
+  //   videoPlayer(videoWrapper);
+  // });
 
-  function videoPlayer(videoWrapper) {
-    let playBtn = videoWrapper.querySelector('.video-playpause');
-    let video = videoWrapper.querySelector('video');
+  // function videoPlayer(videoWrapper) {
+  //   let playBtn = videoWrapper?.querySelector('.video-playpause');
+  //   let video = videoWrapper?.querySelector('.video-frame');
 
-    video.addEventListener('click', (e) => {
-      // if (e.target === video) {
-      //   playBtn.classList.toggle('hide');
-      // }
-      if (playBtn && video) {
-        video.paused ? playBtn.classList.add('hide') : playBtn.classList.remove('hide');
-      }
-    })
-  }
+  //   video?.addEventListener('click', (e) => {
+  //     // if (e.target === video) {
+  //     //   playBtn.classList.toggle('hide');
+  //     // }
+  //     if (playBtn && video) {
+  //       video.paused ? playBtn.classList.add('hide') : playBtn.classList.remove('hide');
+  //     }
+  //   })
+  // }
 
-  reviewSwiper.on('slideChange', function () {
-    videoWrappers.forEach(videoWrapper => {
-      let video = videoWrapper.querySelector('video');
-      let playBtn = videoWrapper.querySelector('.video-playpause');
-      video.pause();
-      if (playBtn && video) {
-        video.paused ? playBtn.classList.remove('hide') : playBtn.classList.add('hide');
-      }
-    })
-  });
+  // reviewSwiper.on('slideChange', function () {
+  //   videoWrappers?.forEach(videoWrapper => {
+  //     let video = videoWrapper?.querySelector('.video-frame');
+  //     let playBtn = videoWrapper?.querySelector('.video-playpause');
+  //     video.pause();
+  //     if (playBtn && video) {
+  //       video?.paused ? playBtn.classList.remove('hide') : playBtn.classList.add('hide');
+  //     }
+  //   })
+  // });
 }
 export default videoSlider;
